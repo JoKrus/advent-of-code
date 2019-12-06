bad5f3a4cb6cd572fd2e391af5fd32edb62ada8b
JoKrus <JoKr1999@web.de>
2019-12-06 17:18:31 +0100

    Day 6

--- src/main/java/de/djgames/jonas/adventofcode/App.java
+++ src/main/java/de/djgames/jonas/adventofcode/App.java
@@ -1,7 +1,5 @@
 package de.djgames.jonas.adventofcode;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
@@ -17,48 +15,20 @@ public class App {
         Reflections reflections = new Reflections("de.djgames.jonas.adventofcode");
         TreeSet<Class<? extends Day>> challenges = new TreeSet<>(Comparator.comparing(Class::getSimpleName));
         challenges.addAll(reflections.getSubTypesOf(Day.class));
-        
+
+//        runSingle(Day2019_6.class);
         runAll(challenges);
     }
 
-    public static void runAll(TreeSet<Class<? extends Day>> challenges) {
-        challenges.forEach(day -> {
-            try {
-                System.out.println(day.getConstructor().newInstance().execute());
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException ignored) {
-            }
-        });
+    public static void runSingle(Class<? extends Day> dayClass) {
+        try {
+            System.out.println(dayClass.getConstructor().newInstance().execute());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 
-
-    static class Node {
-        TreeSet<String> requires = new TreeSet<>();
-        TreeSet<String> enables = new TreeSet<>();
-        String ident;
-        int duration;
-
-        public Node(String ident) {
-            this.ident = ident;
-            if (!ident.isEmpty())
-                duration = 60 + ident.toCharArray()[0] + 1 - 'A';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Node node = (Node) o;
-            return new EqualsBuilder().append(ident, node.ident).isEquals();
-        }
-
-        @Override
-        public int hashCode() {
-            return new HashCodeBuilder(17, 37).append(ident).toHashCode();
-        }
-
-        @Override
-        public String toString() {
-            return "Node{" + "requires=" + requires + ", enables=" + enables + ", ident='" + ident + '\'' + '}';
-        }
+    public static void runAll(TreeSet<Class<? extends Day>> challenges) {
+        challenges.forEach(App::runSingle);
     }
 }
---
+++ src/main/java/de/djgames/jonas/adventofcode/aoc2019/Day2019_6.java
@@ -0,0 +1,154 @@
+package de.djgames.jonas.adventofcode.aoc2019;
+
+import de.djgames.jonas.adventofcode.Day;
+import org.apache.commons.lang3.tuple.Triple;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Day2019_6 extends Day {
+    @Override
+    public String part1Logic() {
+        Triple<Node, List<Node>, Map<String, List<String>>> things = getThings();
+
+        int orbitcount = things.getMiddle().stream().mapToInt(Node::getCount).sum();
+
+        return String.format("%d", orbitcount);
+    }
+
+    @Override
+    public String part2Logic() {
+        Triple<Node, List<Node>, Map<String, List<String>>> triple = getThings();
+
+        Node you = triple.getMiddle().stream().filter(node -> node.self.equals("YOU")).findFirst().get();
+        Node san = triple.getMiddle().stream().filter(node -> node.self.equals("SAN")).findFirst().get();
+
+        int dist = 0;
+        while (!you.parent.equals(san.parent)) {
+            Node n;
+            if ((n = you.parent.deepContains(san)) != null) {
+                you.moveNodeToChild(n);
+            } else {
+                you.moveNodeToParent();
+            }
+            dist++;
+        }
+
+        return String.format("%d", dist);
+    }
+
+    private Triple<Node, List<Node>, Map<String, List<String>>> getThings() {
+        String[] orbitList = input.split("\n");
+        Map<String, List<String>> orbitter = new HashMap<>();
+
+        for (int i = 0; i < orbitList.length; i++) {
+            String[] arg = orbitList[i].split(Pattern.quote(")"));
+            List<String> vals = orbitter.getOrDefault(arg[0], new ArrayList<>());
+            vals.add(arg[1]);
+            orbitter.put(arg[0], vals);
+        }
+
+        Node root = new Node(null, "COM", orbitter);
+
+        List<Node> nodes = new ArrayList<>();
+        root.getAllNodes(nodes);
+
+        return Triple.of(root, nodes, orbitter);
+    }
+
+    private static class Node {
+        Node parent;
+        String self;
+        List<Node> childs;
+
+        public Node(Node parent, String self, Map<String, List<String>> orbitter) {
+            this.parent = parent;
+            this.self = self;
+            childs = new ArrayList<>();
+            if (orbitter.get(self) != null && !orbitter.get(self).isEmpty()) {
+                orbitter.get(self).forEach(s -> this.childs.add(new Node(this, s, orbitter)));
+            }
+        }
+
+        public int getCount() {
+            int[] ret = new int[]{0};
+            getCount(ret);
+            return ret[0];
+        }
+
+        private int[] getCount(int[] i) {
+            if (parent == null) {
+                return i;
+            } else {
+                i[0]++;
+                return parent.getCount(i);
+            }
+        }
+
+        public void getAllNodes(List<Node> list) {
+            list.add(this);
+            if (!childs.isEmpty()) {
+                childs.forEach(node -> node.getAllNodes(list));
+            }
+        }
+
+        private Node deepContains(Node n) {
+            Node ret = null;
+            if (childs.contains(n)) {
+                return this;
+            } else if (!this.childs.isEmpty()) {
+                for (Node start : this.childs) {
+                    boolean[] boolref = {false};
+                    start.deepContains(n, boolref);
+                    if (boolref[0])
+                        return start;
+                }
+            }
+            return ret;
+        }
+
+        private void deepContains(Node n, boolean[] found) {
+            if (childs.contains(n)) {
+                found[0] = true;
+            } else if (!this.childs.isEmpty()) {
+                for (Node node : childs) {
+                    node.deepContains(n, found);
+                }
+            }
+        }
+
+        public void moveNodeToParent() {
+            this.parent.parent.childs.add(this);
+            this.parent.childs.remove(this);
+            this.parent = parent.parent;
+        }
+
+        public void moveNodeToChild(Node child) {
+            parent.childs.remove(this);
+            child.childs.add(this);
+            this.parent = child;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return Objects.equals(self, node.self);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(self);
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "self='" + self + '\'' +
+                    ", childs=" + childs.stream().map(node -> node.self).collect(Collectors.toList()) +
+                    '}';
+        }
+    }
+}
