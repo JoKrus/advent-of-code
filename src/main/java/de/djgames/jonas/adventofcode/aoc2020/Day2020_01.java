b3bbe53b09d3c5e68c956755201e1db2dd9abf48
JoKrus <JoKrus@web.de>
2020-12-01 07:33:15 +0100

    Day one done
    Signed-off-by: JoKrus <JoKrus@web.de>

--- src/main/java/de/djgames/jonas/adventofcode/aoc2020/Day2020_01.java
+++ src/main/java/de/djgames/jonas/adventofcode/aoc2020/Day2020_01.java
@@ -2,14 +2,38 @@ package de.djgames.jonas.adventofcode.aoc2020;
 
 import de.djgames.jonas.adventofcode.Day;
 
+import java.util.Arrays;
+
+
 public class Day2020_01 extends Day {
     @Override
     public String part1Logic() {
+        var ints = Arrays.stream(input.split("\n")).mapToInt(Integer::parseInt).toArray();
+
+        for (var inte : ints) {
+            for (var inte2 : ints) {
+                if (inte + inte2 == 2020) {
+                    return "" + (inte * inte2);
+                }
+            }
+        }
+
         return null;
     }
 
     @Override
     public String part2Logic() {
+        var ints = Arrays.stream(input.split("\n")).mapToInt(Integer::parseInt).toArray();
+
+        for (var inte : ints) {
+            for (var inte2 : ints) {
+                for (var inte3 : ints) {
+                    if (inte + inte2 + inte3 == 2020) {
+                        return "" + (inte * inte2 * inte3);
+                    }
+                }
+            }
+        }
         return null;
     }
 }
