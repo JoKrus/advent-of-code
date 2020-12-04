645b5050c82d0384e267cb0013c7a23247ab88b2
JoKrus <JoKrus@web.de>
2020-12-04 21:44:58 +0100

    implemented day 4 kinda bad
    Signed-off-by: JoKrus <JoKrus@web.de>

--- src/main/java/de/djgames/jonas/adventofcode/App.java
+++ src/main/java/de/djgames/jonas/adventofcode/App.java
@@ -1,6 +1,6 @@
 package de.djgames.jonas.adventofcode;
 
-import de.djgames.jonas.adventofcode.aoc2020.Day2020_03;
+import de.djgames.jonas.adventofcode.aoc2020.Day2020_05;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
@@ -36,7 +36,7 @@ public class App {
         challenges.addAll(reflections.getSubTypesOf(Day.class));
 
         //runAllTime(challenges);
-        runSingleTime(Day2020_03.class);
+        runSingleTime(Day2020_05.class);
     }
 
     public static void runSinglePart(Class<? extends Day> dayClass, boolean part1) {
