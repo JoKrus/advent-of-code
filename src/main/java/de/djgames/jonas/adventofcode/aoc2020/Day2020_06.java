95223e60c26dab7969c68c04a7c577edd419d92a
JoKrus <JoKrus@web.de>
2020-12-05 12:45:36 +0100

    day 6 prep
    Signed-off-by: JoKrus <JoKrus@web.de>

--- src/main/java/de/djgames/jonas/adventofcode/aoc2020/Day2020_06.java
+++ src/main/java/de/djgames/jonas/adventofcode/aoc2020/Day2020_06.java
@@ -1,4 +1,15 @@
 package de.djgames.jonas.adventofcode.aoc2020;
 
-public class Day2020_06 {
+import de.djgames.jonas.adventofcode.Day;
+
+public class Day2020_06 extends Day {
+    @Override
+    public String part1Logic() {
+        return null;
+    }
+
+    @Override
+    public String part2Logic() {
+        return null;
+    }
 }
