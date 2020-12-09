72a28d481e48d7a1d8bcf9da1e07ffd8eb324a3d
JoKrus <JoKrus@web.de>
2020-12-09 13:02:02 +0100

    Day 9 done
    Signed-off-by: JoKrus <JoKrus@web.de>

---
+++ src/main/java/de/djgames/jonas/adventofcode/aoc2020/Day2020_10.java
@@ -0,0 +1,15 @@
+package de.djgames.jonas.adventofcode.aoc2020;
+
+import de.djgames.jonas.adventofcode.Day;
+
+public class Day2020_10 extends Day {
+    @Override
+    public String part1Logic() {
+        return null;
+    }
+
+    @Override
+    public String part2Logic() {
+        return null;
+    }
+}
