1792b1f8a02a7006d82d12d8703d3188cacba8ba
JoKrus <JoKrus@web.de>
2020-12-10 12:51:20 +0100

    implemented day 10
    Signed-off-by: JoKrus <JoKrus@web.de>

--- src/main/java/de/djgames/jonas/adventofcode/App.java
+++ src/main/java/de/djgames/jonas/adventofcode/App.java
@@ -1,6 +1,6 @@
 package de.djgames.jonas.adventofcode;
 
-import de.djgames.jonas.adventofcode.aoc2020.Day2020_10;
+import de.djgames.jonas.adventofcode.aoc2020.Day2020_11;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
@@ -36,7 +36,7 @@ public class App {
         challenges.addAll(reflections.getSubTypesOf(Day.class));
 
         //runAllTime(challenges);
-        runSingleTime(Day2020_10.class);
+        runSingleTime(Day2020_11.class);
     }
 
     public static void runSinglePart(Class<? extends Day> dayClass, boolean part1) {
