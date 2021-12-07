3bb3a7f65e32aec3b497c7f503db33c242f6beb1
JoKrus <JoKrus@web.de>
2021-12-07 12:02:01 +0100

    Day 7
    Signed-off-by: JoKrus <JoKrus@web.de>

--- src/main/java/net/jcom/adventofcode/aoc2021/Day2021_07.java
+++ src/main/java/net/jcom/adventofcode/aoc2021/Day2021_07.java
@@ -2,14 +2,51 @@ package net.jcom.adventofcode.aoc2021;
 
 import net.jcom.adventofcode.Day;
 
+import java.util.Arrays;
+
 public class Day2021_07 extends Day {
     @Override
     public String part1Logic() {
-        return null;
+        var inputInt = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).sorted().toArray();
+
+        int lowestFuel = Integer.MAX_VALUE;
+        int lowestFuelAt = -1;
+        for (int alignCoordinate = inputInt[0]; alignCoordinate < inputInt[inputInt.length - 1]; ++alignCoordinate) {
+            int fuel = 0;
+            for (int num :
+                    inputInt) {
+                fuel += Math.abs(num - alignCoordinate);
+            }
+            if (fuel < lowestFuel) {
+                lowestFuel = fuel;
+                lowestFuelAt = alignCoordinate;
+            }
+
+        }
+
+        return "%d".formatted(lowestFuel);
     }
 
     @Override
     public String part2Logic() {
-        return null;
+        var inputInt = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).sorted().toArray();
+
+        int lowestFuel = Integer.MAX_VALUE;
+        int lowestFuelAt = -1;
+        for (int alignCoordinate = inputInt[0]; alignCoordinate < inputInt[inputInt.length - 1]; ++alignCoordinate) {
+            int fuel = 0;
+            for (int num :
+                    inputInt) {
+                int dist = Math.abs(num - alignCoordinate);
+                fuel += (dist * dist + dist) / 2;
+            }
+            if (fuel < lowestFuel) {
+                lowestFuel = fuel;
+                lowestFuelAt = alignCoordinate;
+            }
+
+        }
+
+        return "%d".formatted(lowestFuel);
     }
 }
