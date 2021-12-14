bc37bf139540c41aca57980e491699e0e69f37da
JoKrus <JoKrus@web.de>
2021-12-14 14:24:21 +0100

    INSANE new function learned
    Map::merge is awesome
    
    Huge thanks to @ProfSchmergmann
    
    Signed-off-by: JoKrus <JoKrus@web.de>

--- src/main/java/net/jcom/adventofcode/aoc2021/Day2021_14.java
+++ src/main/java/net/jcom/adventofcode/aoc2021/Day2021_14.java
@@ -21,7 +21,7 @@ public class Day2021_14 extends Day {
             polyInstrMap.put(split1[0], split1[1]);
         });
 
-        Map<String, Long> currentOccurences =
+        Map<String, Long> currentOccurrences =
                 Arrays.stream(polymer.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         Map<String, Long> currentPairs = new HashMap<>();
         for (int i = 0; i < polymer.length() - 1; i++) {
@@ -30,15 +30,15 @@ public class Day2021_14 extends Day {
         }
 
         for (int i = 0; i < 10; i++) {
-            polyInteration2(currentOccurences, currentPairs, polyInstrMap);
+            polyIteration2(currentOccurrences, currentPairs, polyInstrMap);
         }
 
-        var ret = currentOccurences.values().stream().mapToLong(Long::longValue).max().getAsLong() -
-                currentOccurences.values().stream().mapToLong(Long::longValue).min().getAsLong();
+        var ret = currentOccurrences.values().stream().mapToLong(Long::longValue).max().getAsLong() -
+                currentOccurrences.values().stream().mapToLong(Long::longValue).min().getAsLong();
         return "%d".formatted(ret);
     }
 
-    private String polyInteration(String polymer, HashMap<String, String> polyInstrMap) {
+    private String polyIteration(String polymer, HashMap<String, String> polyInstrMap) {
         StringBuilder sb = new StringBuilder(polymer);
         for (int i = 0; i < polymer.length() - 1; i++) {
             String sub = polymer.substring(i, i + 2);
@@ -60,33 +60,33 @@ public class Day2021_14 extends Day {
             polyInstrMap.put(split1[0], split1[1]);
         });
 
-        Map<String, Long> currentOccurences =
+        Map<String, Long> currentOccurrences =
                 Arrays.stream(polymer.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         Map<String, Long> currentPairs = new HashMap<>();
         for (int i = 0; i < polymer.length() - 1; i++) {
             String subSequence = polymer.substring(i, i + 2);
-            currentPairs.put(subSequence, currentPairs.getOrDefault(subSequence, 0L) + 1);
+            //increment by one or set to one
+            currentPairs.merge(subSequence, 1L, Long::sum);
         }
 
         for (int i = 0; i < 40; i++) {
-            polyInteration2(currentOccurences, currentPairs, polyInstrMap);
+            polyIteration2(currentOccurrences, currentPairs, polyInstrMap);
         }
 
-        var ret = currentOccurences.values().stream().mapToLong(Long::longValue).max().getAsLong() -
-                currentOccurences.values().stream().mapToLong(Long::longValue).min().getAsLong();
+        var ret = currentOccurrences.values().stream().mapToLong(Long::longValue).max().getAsLong() -
+                currentOccurrences.values().stream().mapToLong(Long::longValue).min().getAsLong();
         return "%d".formatted(ret);
     }
 
-    private void polyInteration2(Map<String, Long> currentOccurences, Map<String, Long> currentPairs, HashMap<String, String> polyInstrMap) {
+    private void polyIteration2(Map<String, Long> currentOccurrences, Map<String, Long> currentPairs, HashMap<String, String> polyInstrMap) {
         HashMap<String, Long> currentPairsCopy = new HashMap<>(currentPairs);
         for (var entryPair : currentPairsCopy.entrySet()) {
             var chars = entryPair.getKey().split("");
             var result = polyInstrMap.get(entryPair.getKey());
-            currentOccurences.put(result, currentOccurences.getOrDefault(result, 0L) + entryPair.getValue());
-            currentPairs.put(entryPair.getKey(), Math.max(0,
-                    currentPairs.get(entryPair.getKey()) - entryPair.getValue()));
-            currentPairs.put(chars[0] + result, currentPairs.getOrDefault(chars[0] + result, 0L) + entryPair.getValue());
-            currentPairs.put(result + chars[1], currentPairs.getOrDefault(result + chars[1], 0L) + entryPair.getValue());
+            currentOccurrences.merge(result, entryPair.getValue(), Long::sum);
+            currentPairs.merge(entryPair.getKey(), -entryPair.getValue(), Long::sum);
+            currentPairs.merge(chars[0] + result, entryPair.getValue(), Long::sum);
+            currentPairs.merge(result + chars[1], entryPair.getValue(), Long::sum);
         }
     }
 }
