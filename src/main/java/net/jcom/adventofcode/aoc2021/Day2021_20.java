15c8cf253e12ebf5180185eda0b076992ac3d6a1
JoKrus <JoKrus@web.de>
2021-12-20 18:02:38 +0100

    optimised day 20
    Signed-off-by: JoKrus <JoKrus@web.de>

--- src/main/java/net/jcom/adventofcode/aoc2021/Day2021_20.java
+++ src/main/java/net/jcom/adventofcode/aoc2021/Day2021_20.java
@@ -1,11 +1,12 @@
 package net.jcom.adventofcode.aoc2021;
 
 import net.jcom.adventofcode.Day;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
-import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day2021_20 extends Day {
@@ -37,15 +38,10 @@ public class Day2021_20 extends Day {
 
     private HashMap<Point, Character> iterateEnhancement(HashMap<Point, Character> imagePixelsMap,
                                                          String enhance, int iteration) {
-        int xMin = imagePixelsMap.keySet().stream().mapToInt(value -> value.x).min().getAsInt();
-        int xMax = imagePixelsMap.keySet().stream().mapToInt(value -> value.x).max().getAsInt();
-        int yMin = imagePixelsMap.keySet().stream().mapToInt(value -> value.y).min().getAsInt();
-        int yMax = imagePixelsMap.keySet().stream().mapToInt(value -> value.y).max().getAsInt();
         char border = iteration % 2 == 1 ? enhance.charAt(0) : enhance.charAt(enhance.length() - 1);
 
         HashMap<Point, Character> ret = new HashMap<>();
         for (var entry : imagePixelsMap.keySet().stream().distinct()
-                .map(this::getNeighborList).flatMap(Collection::stream)
                 .map(this::getNeighborList).flatMap(Collection::stream)
                 .collect(Collectors.toSet())) {
             var bin = getBinaryString(entry, imagePixelsMap, border);
@@ -56,41 +52,6 @@ public class Day2021_20 extends Day {
         return ret;
     }
 
-
-    private Pair<HashSet<Point>, Character> iterateEnhancement(Pair<HashSet<Point>, Character> imagePixelsPair,
-                                                               String enhance) {
-        HashSet<Point> ret = new HashSet<>();
-        var imagePixels = imagePixelsPair.getLeft();
-        var newCheck = imagePixelsPair.getRight() == '#' ? '.' : '#';
-
-        for (var entry : imagePixels.stream().distinct()
-                .map(this::getNeighborList).flatMap(Collection::stream)
-                .map(this::getNeighborList).flatMap(Collection::stream)
-                .map(this::getNeighborList).flatMap(Collection::stream)
-                .collect(Collectors.toSet())) {
-            var bin = getBinaryString(entry, imagePixels);
-            var number = Integer.parseInt(bin, 2);
-            if (enhance.charAt(number) == newCheck) {
-                ret.add(entry);
-            }
-        }
-
-        return Pair.of(ret, newCheck);
-    }
-
-    private String getBinaryString(Point entry, HashSet<Point> imagePixels) {
-        StringBuilder sb = new StringBuilder();
-        int[] dx = new int[]{-1, 0, 1, -1, 0, 1, -1, 0, 1};
-        int[] dy = new int[]{-1, -1, -1, 0, 0, 0, 1, 1, 1};
-
-        for (int i = 0; i < dx.length; i++) {
-            var p = new Point(entry.x + dx[i], entry.y + dy[i]);
-            var charString = imagePixels.contains(p);
-            sb.append(charString ? "1" : "0");
-        }
-
-        return sb.toString();
-    }
 
     private String getBinaryString(Point entry, HashMap<Point, Character> imagePixelsMap, char border) {
         StringBuilder sb = new StringBuilder();
