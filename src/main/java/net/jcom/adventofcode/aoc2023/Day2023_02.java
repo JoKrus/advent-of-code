f5402dbfea0bec8b1a759f945a2f1b3680afd289
JoKrus <JoKrus@web.de>
2023-12-01 11:18:56 +0100

    prep day2
    Signed-off-by: JoKrus <JoKrus@web.de>

---
+++ src/main/java/net/jcom/adventofcode/aoc2023/Day2023_02.java
@@ -0,0 +1,15 @@
+package net.jcom.adventofcode.aoc2023;
+
+import net.jcom.adventofcode.Day;
+
+public class Day2023_02 extends Day {
+    @Override
+    public String part1Logic() {
+        return null;
+    }
+
+    @Override
+    public String part2Logic() {
+        return null;
+    }
+}
